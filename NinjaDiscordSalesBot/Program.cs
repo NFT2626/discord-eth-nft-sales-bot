036ec52bffb4688a225df02f020e75c1fecf3f06
NFT2626 <96617076+NFT2626@users.noreply.github.com>
2022-05-12 11:58:23 +0100

    Add files via upload

--- NinjaDiscordSalesBot/Program.cs
+++ NinjaDiscordSalesBot/Program.cs
@@ -6,10 +6,10 @@
 var bot = new NinjaBot(new NinjaBotOptions
 {
     DiscordBotToken = "<discord_bot_token>",
-    DiscordChannelId = "<discord_channel_id>",
-    DiscordWebhookUrl = "<discord_webhook_url>",
-    CollectionContractAddress = "<collection_contract_address>",
-    InfuraApiKey = "<infura_api_key>"
+    DiscordChannelId = "974258301384675348",
+    DiscordWebhookUrl = "https://discord.com/api/webhooks/974263265192468501/z29DvTy2OJGtYpCaS4tPHwcggEVsVa0UkJt-URw_EjS8pHPTfHvXZhKISFNz-lWxFvJo",
+    CollectionContractAddress = "0x031920cc2D9F5c10B444FD44009cd64F829E7be2",
+    InfuraApiKey = "https://mainnet.infura.io/v3/3de6ca64c8524650b7a2230f357251ab"
 });
 
 await bot.StartAsync();
